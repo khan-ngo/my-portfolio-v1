c48ea13219515419428bdfde0b679148a17024fb
khan <khan.ngo@gmail.com>
2020-04-22 11:06:33 -0400

    Migrates Training to its own component

---
+++ src/components/Training/Training.js
@@ -0,0 +1,55 @@
+import React, { Component } from "react";
+import "./Training.css";
+
+export default class Training extends Component {
+  render() {
+    return (
+      <section id="sec">
+        <h2>TEST</h2>
+        <ul>
+          <li>
+            <span class="fa fa-code" />
+
+            <h3>Music</h3>
+            <p>
+              Music is an art form whose medium is sound and silence. Generally,
+              a song is considered the smallest standalone work of music,
+              especially when involving singing.
+            </p>
+          </li>
+          <li>
+            <span class="fa fa-music" />
+            <h3>Music</h3>
+            <p>
+              Music is an art form whose medium is sound and silence. Generally,
+              a song is considered the smallest standalone work of music,
+              especially when involving singing.{" "}
+            </p>
+            <p>
+              The common elements of music are pitch (which governs melody and
+              harmony), rhythm (and its associated concepts tempo, meter, and
+              articulation), dynamics, and the sonic qualities of timbre and
+              texture.
+            </p>
+          </li>
+          <li>
+            <span class="fa fa-users" />
+            <h3>Music</h3>
+            <p>
+              Music is an art form whose medium is sound and silence. Generally,
+              a song is considered the smallest standalone work of music,
+              especially when involving singing.{" "}
+            </p>
+            <p>
+              The common elements of music are pitch (which governs melody and
+              harmony), rhythm (and its associated concepts tempo, meter, and
+              articulation), dynamics, and the sonic qualities of timbre and
+              texture. The word derives from Greek μουσική (mousike; "art of the
+              Muses").
+            </p>
+          </li>
+        </ul>
+      </section>
+    );
+  }
+}
