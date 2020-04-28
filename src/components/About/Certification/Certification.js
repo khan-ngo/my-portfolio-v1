041b84b1cf7bbc90d356c917adee4866991825b0
khan <khan.ngo@gmail.com>
2020-04-27 23:29:10 -0400

    Add Certification component

---
+++ src/components/About/Certification/Certification.js
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+import { Element } from "react-scroll";
+
+import {
+  ProjectContainer,
+  ProjectHeader,
+  ContentWrapper,
+} from "../Experience/ExperienceStyles";
+
+class Experience extends Component {
+  state = {};
+
+  render() {
+    return (
+      <Element name="Projects">
+        <ProjectContainer>
+          <ProjectHeader>Work Experience</ProjectHeader>
+
+          <ContentWrapper />
+        </ProjectContainer>
+      </Element>
+    );
+  }
+}
+
+export default Experience;
