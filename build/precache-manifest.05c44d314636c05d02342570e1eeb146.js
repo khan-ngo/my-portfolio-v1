60eac91e3bc34f8feda3c47ba049200a59ab0ff5
khan <khan.ngo@gmail.com>
2020-07-21 00:58:44 -0400

    update state of RI

---
+++ build/precache-manifest.05c44d314636c05d02342570e1eeb146.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "22e116c2e152efaec741",
+    "url": "/my-portfolio-v1/static/css/main.341f1733.chunk.css"
+  },
+  {
+    "revision": "22e116c2e152efaec741",
+    "url": "/my-portfolio-v1/static/js/main.22e116c2.chunk.js"
+  },
+  {
+    "revision": "4fdd7a446771d53dde8e",
+    "url": "/my-portfolio-v1/static/css/1.02d94356.chunk.css"
+  },
+  {
+    "revision": "4fdd7a446771d53dde8e",
+    "url": "/my-portfolio-v1/static/js/1.4fdd7a44.chunk.js"
+  },
+  {
+    "revision": "1bf9373859563fcd5c9a",
+    "url": "/my-portfolio-v1/static/js/runtime~main.1bf93738.js"
+  },
+  {
+    "revision": "25009a69ca2fbf770a3aa09fc85c7e90",
+    "url": "/my-portfolio-v1/static/media/headshot.25009a69.png"
+  },
+  {
+    "revision": "e7b43349d0aef044464006d567397f59",
+    "url": "/my-portfolio-v1/static/media/Khanh_Ngo_Resume.e7b43349.pdf"
+  },
+  {
+    "revision": "4e65d79090b2638436e1af3ee96f7c64",
+    "url": "/my-portfolio-v1/static/media/headshot-bw.4e65d790.png"
+  },
+  {
+    "revision": "b6e72be4d553e8334d0fa8c36f24b975",
+    "url": "/my-portfolio-v1/index.html"
+  }
+];
